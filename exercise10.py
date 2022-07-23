b10123eb86ff25f7e52618119ca796a2438b3287
royal1104 <78022971+royal1104@users.noreply.github.com>
2022-07-23 12:40:16 +0530

    Set Operations

---
+++ exercise10.py
@@ -0,0 +1,17 @@
+# Program to illustrate different set operations 
+
+# define three sets
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+# union of set
+print("Union of E and N is",E | N)
+
+#intersection of set
+print("Intersection of E and N is",E & N)
+
+#difference of set
+print("Difference of E and N is",E - N)
+
+#symmetric difference of set
+print("Symmetric difference of E and N is",E ^ N)
